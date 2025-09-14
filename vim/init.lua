ef9782790a730766377d680896eb4f71053cf6df
Jazastry <jazastry@gmail.com>
2025-09-14 19:13:05 +0200

    feat: adds search and replace plugins

--- vim/init.lua
+++ vim/init.lua
@@ -68,3 +68,16 @@ vim.api.nvim_create_user_command('Restart', function()
     vim.cmd('qall!')
   end
 end, {})
+
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key",
+      }
+    }
+  }
+}
